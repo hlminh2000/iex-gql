1f2ec6fec645914138a4d92b4793c72d4370063f
Minh <hlminh2000@gmail.com>
2018-10-07 08:57:39 -0400

    sets up initial few queries

---
+++ index.js
@@ -0,0 +1,109 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const {
+  createCompanyInfoLoader,
+  createFinancialReportLoader,
+  createPeersLoader,
+  createSectorQuotesLoader
+} = require("./services/iex");
+
+const typeDefs = gql`
+  type FinancialReport {
+    reportDate: String
+    grossProfit: Float
+    costOfRevenue: Float
+    operatingRevenue: Float
+    totalRevenue: Float
+    operatingIncome: Float
+    netIncome: Float
+    researchAndDevelopment: Float
+    operatingExpense: Float
+    currentAssets: Float
+    totalAssets: Float
+    totalLiabilities: Float
+    currentCash: Float
+    currentDebt: Float
+    totalCash: Float
+    totalDebt: Float
+    shareholderEquity: Float
+    cashChange: Float
+    cashFlow: Float
+    operatingGainsLosses: Float
+  }
+
+  type Stock {
+    ticker: String
+    companyName: String
+    exchange: String
+    industry: String
+    website: String
+    description: String
+    CEO: String
+    issueType: String
+    sector: Sector
+    tags: [String]
+    financials: [FinancialReport]
+    peers: [Stock]
+  }
+
+  type Sector {
+    key: String
+    constituents(first: Int = 10, offset: Int = 0): [Stock]
+  }
+
+  type Query {
+    stock(ticker: String!): Stock
+    sector(sectorName: String!): Sector
+  }
+`;
+
+const resolveSector = (_, { sectorName }, context, info) => ({
+  key: sectorName,
+  constituents: async ({ first, offset }, context) =>
+    (await context.loaders.sectorQuotesLoader.load(sectorName))
+      .slice(offset, first)
+      .map(({ symbol }) => resolveStock(_, { ticker: symbol }, context, info))
+});
+
+const resolveStock = async (_, { ticker }, context, info) => {
+  const {
+    loaders: { companyInfoLoader, financialReportLoader, peersLoader }
+  } = context;
+  const companyInfo = await companyInfoLoader.load(ticker);
+  const resolvePeers = async () =>
+    (await peersLoader.load(ticker)).map(ticker =>
+      resolveStock(_, { ticker }, context, info)
+    );
+  return {
+    ...companyInfo,
+    ticker: ticker.toUpperCase(),
+    financials: financialReportLoader.load(ticker),
+    sector: (_, args, context) =>
+      resolveSector(_, { sectorName: companyInfo.sector }, context, info),
+    peers: resolvePeers
+  };
+};
+
+const resolvers = {
+  Query: {
+    stock: resolveStock,
+    sector: resolveSector
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: () => ({
+    loaders: {
+      companyInfoLoader: createCompanyInfoLoader(),
+      financialReportLoader: createFinancialReportLoader(),
+      peersLoader: createPeersLoader(),
+      sectorQuotesLoader: createSectorQuotesLoader()
+    }
+  })
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
---
+++ services/iex.js
@@ -0,0 +1,73 @@
+const RateLimit = require("async-sema/rate-limit");
+const fetch = require("isomorphic-fetch");
+const retry = require("async-retry");
+const DataLoader = require("dataloader");
+
+const IEX_URL = `https://api.iextrading.com/1.0`;
+
+const iexRateLimit = RateLimit(100);
+
+const iexFetch = async (...args) => {
+  return retry(
+    async bail => {
+      await iexRateLimit();
+      return fetch(...args);
+    },
+    { retries: 5 }
+  );
+};
+
+const toUrl = ticker => encodeURI(ticker.toLowerCase());
+
+const createCompanyInfoLoader = () =>
+  new DataLoader(tickers =>
+    Promise.all(
+      tickers.map(ticker =>
+        iexFetch(`${IEX_URL}/stock/${toUrl(ticker)}/company`).then(res =>
+          res.json()
+        )
+      )
+    )
+  );
+
+const createFinancialReportLoader = () =>
+  new DataLoader(tickers =>
+    Promise.all(
+      tickers.map(ticker =>
+        iexFetch(`${IEX_URL}/stock/${toUrl(ticker)}/financials`)
+          .then(res => res.json())
+          .then(({ financials }) => financials)
+      )
+    )
+  );
+
+const createPeersLoader = () =>
+  new DataLoader(tickers =>
+    Promise.all(
+      tickers.map(ticker =>
+        iexFetch(`${IEX_URL}/stock/${toUrl(ticker)}/peers`).then(res =>
+          res.json()
+        )
+      )
+    )
+  );
+
+const createSectorQuotesLoader = () =>
+  new DataLoader(sectors =>
+    Promise.all(
+      sectors.map(sector =>
+        iexFetch(
+          `${IEX_URL}/stock/market/collection/sector?collectionName=${encodeURI(
+            sector
+          )}`
+        ).then(res => res.json())
+      )
+    )
+  );
+
+module.exports = {
+  createCompanyInfoLoader,
+  createFinancialReportLoader,
+  createPeersLoader,
+  createSectorQuotesLoader
+};
