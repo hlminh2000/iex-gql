20ac5f079743ba627f1d92c212186254dc5e8845
Minh Ha <hlminh2000@gmail.com>
2018-10-08 17:32:39 -0400

    supports multiple proxies and applies rate limit in proxy instance (#2)

--- services/iexProxy.js
+++ services/iexProxy.js
@@ -1,3 +1,4 @@
+const RateLimit = require("async-sema/rate-limit");
 const express = require("express");
 const fetch = require("isomorphic-fetch");
 const PORT = process.env.PORT || 5000;
@@ -6,13 +7,17 @@ const IEX_URL = `https://api.iextrading.com/1.0`;
 
 const app = express();
 
+const iexRateLimit = RateLimit(100);
+
 app.get("*", (req, res) => {
-  fetch(`${IEX_URL}${req.originalUrl}`)
-    .then(res => res.json())
-    .catch(err => {})
-    .then(stuff => {
-      res.send(stuff);
-    });
+  iexRateLimit().then(() =>
+    fetch(`${IEX_URL}${req.originalUrl}`)
+      .then(res => res.json())
+      .catch(err => {})
+      .then(stuff => {
+        res.send(stuff);
+      })
+  );
 });
 
 app.listen(PORT, () => {
