70e3722dcb3c20c1d41d719b605328a85881fac0
Minh <hlminh2000@gmail.com>
2018-10-08 17:31:19 -0400

    supports multiple proxies and applies rate limit in proxy instance

--- services/iex.js
+++ services/iex.js
@@ -4,16 +4,24 @@ const retry = require("async-retry");
 const DataLoader = require("dataloader");
 
 const IEX_URL = `https://api.iextrading.com/1.0`;
-const IEX_PROXY = process.env.IEX_PROXY || `http://localhost:5000`;
+const IEX_PROXIES = process.env.IEX_PROXIES || `http://localhost:5000`;
+
+const iexProxies = IEX_PROXIES.split(" ")
+  .join("")
+  .split(",");
+const pickProxies = proxies =>
+  proxies[Math.floor(Math.random() * proxies.length)];
 
 const iexRateLimit = RateLimit(50);
 
-const coinFlip = () => Math.random() > 0.5;
+const coinFlip = () => Math.random() > 1 / (iexProxies.length + 1);
 
 const iexFetch = async (url, ...args) => {
   const usingProxy = coinFlip();
-  const fetchUrl = !usingProxy ? url : url.split(IEX_URL).join(IEX_PROXY);
-  const fetchId = `${url}_${Math.random()}${usingProxy ? "_PROXY" : ""}`;
+  const fetchUrl = !usingProxy
+    ? url
+    : url.split(IEX_URL).join(pickProxies(iexProxies));
+  const fetchId = `${url}:::${Math.random()}${usingProxy ? ":::PROXY" : ""}`;
   console.time(fetchId);
   return retry(
     async bail => {
