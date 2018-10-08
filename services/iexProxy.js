02bf84ea6bb2c935bc8f3fc097a0fcd04574026f
Minh <hlminh2000@gmail.com>
2018-10-08 15:37:11 -0400

    adds proxy

---
+++ services/iexProxy.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const fetch = require("isomorphic-fetch");
+const PORT = process.env.PORT || 5000;
+
+const IEX_URL = `https://api.iextrading.com/1.0`;
+
+const app = express();
+
+app.get("*", (req, res) => {
+  fetch(`${IEX_URL}${req.originalUrl}`)
+    .then(res => res.json())
+    .catch(err => {})
+    .then(stuff => {
+      res.send(stuff);
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`⚡️  Proxy ready at http://localhost:${PORT}`);
+});
